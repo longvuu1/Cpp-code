450ce98b1b8908eda8264b0d4cce28d63b88acf1
longvuu <herryvu.dev@gmail.com>
2025-04-01 21:04:18 +0700

    up

--- yendinh1/tìm kiếm nhị phân/ReimuOJ/Mảng con lớn.cpp
+++ yendinh1/tìm kiếm nhị phân/ReimuOJ/Mảng con lớn.cpp
@@ -1,6 +1,5 @@
 /*
     @author longvuuuu
-    @github kuronight29
 */
 #include <bits/stdc++.h>
 #define taskname "main"
@@ -10,15 +9,44 @@
 #define simp main
 #define pb push_back
 using namespace std;
-
-int simp() {
+int simp()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    if(fopen((string(taskname) + ".inp").c_str(), "r") != NULL) {
+    if (fopen((string(taskname) + ".inp").c_str(), "r") != NULL)
+    {
         freopen((string(taskname) + ".inp").c_str(), "r", stdin);
         freopen((string(taskname) + ".out").c_str(), "w", stdout);
     }
-    
-    
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    vector<ll> s(n + 1, 0);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        s[i + 1] = s[i] + a[i];
+    }
+    ll res = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ll t = s[i] + k;
+        ll l = i + 1, r = n + 1, j = n + 1;
+        while (l < r)
+        {
+            ll mid = (l + r) / 2;
+            if (s[mid] >= t)
+            {
+                j = mid;
+                r = mid;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        res += (n + 1) - j;
+    }
+    cout << res << '\n';
     return 0;
 }
